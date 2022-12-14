ca8ce84a7db975fdce14a218dec78ea8043e8dc1
Pann Kaansadich <pannrattanapong@gmail.com>
2022-12-15 00:37:01 +0700

    Home -set route

---
+++ src/components/navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { NavbarContainer, NavLinks, NavLink } from "./navbar.styles";
+
+const Navbar = () => {
+  return (
+    <div>
+      <NavbarContainer>
+        <p>PannKs</p>
+        <NavLinks>
+          <NavLink className="nav-link" to="/shop">
+            Projects
+          </NavLink>
+          <NavLink className="nav-link" to="/auth">
+            Sign in
+          </NavLink>
+        </NavLinks>
+      </NavbarContainer>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="section__header">
+      <h1 className="supertitle">
+        <span className="supertitle__1">My name is </span>
+        <span className="supertitle__2">Pann</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
